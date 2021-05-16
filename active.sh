12578c5ba6e23bc47ce385277c850bde232892b6
Bhavuk <bhavuksharma2202@gmail.com>
2021-05-16 18:47:15 +0530

    camera mic script

---
+++ active.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VID=$(fuser /dev/video0)
+AUD=$(fuser /dev/snd/pcmC0D0c)
+
+function test {
+    function vid {
+        if [[ $VID ]] ; then
+            printf "📸 "
+        fi
+    }
+    
+    function aud {
+        if [[ $AUD ]] ; then 
+            printf "📢"
+        fi
+    }
+    
+    vid
+    aud
+
+}
+
+test
