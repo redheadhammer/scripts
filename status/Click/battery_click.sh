9ad6041910b242761dfa2948ad2016acadedfc6b
Bhavuk <bhavuksharma2202@gmail.com>
2021-05-28 17:16:10 +0530

    modified battery_click

--- status/Click/battery_click.sh
+++ status/Click/battery_click.sh
@@ -2,5 +2,5 @@
 
 STATUS=$(cat /sys/class/power_supply/BAT0/status)
 COUNT=$(cat /sys/class/power_supply/BAT0/cycle_count)
-notify-send "($STATUS)" " $(uptime |awk '{print $3}' | sed "s/,/ /") ($COUNT cycles)" --icon $HOME/scripts/status/Click/icons/uptime.png
+notify-send "($STATUS)" "uptime: $(uptime |awk '{print $3}') ($COUNT cycles)" --icon $HOME/scripts/status/Click/icons/uptime.png
 
