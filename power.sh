c0e9087ff014bdd461a864c22d1211664d9ac9c9
Bhavuk <bhavuksharma2202@gmail.com>
2021-06-02 11:06:06 +0530

    corrected some recent scripts

--- power.sh
+++ power.sh
@@ -2,10 +2,12 @@
 
 status=$(cat /sys/class/power_supply/AC0/online)
 
-if ! [[ -f /tmp/check ]]; then
-    touch /tmp/check
+#echo $(date) >> /tmp/attested
 
-else
+#if ! [[ -f /tmp/check ]]; then
+#    touch /tmp/check
+
+#else
     if [[ $status -eq 0 ]]; then 
         su bhavuksharma2202 -c "DBUS_SESSION_BUS_ADDRESS=\"unix:path=/run/user/1000/bus\" notify-send \"Power\" \"AC Offline\""
         /usr/bin/sudo -u bhavuksharma2202 /usr/bin/aplay /home/bhavuksharma2202/scripts/sounds/disconnected.wav > /dev/null 2>&1
@@ -16,4 +18,4 @@ else
         rm /tmp/check
     fi
 
-fi
+#fi
---
+++ power_menu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIALOG_RESULT=$(echo -e 'hibernate\nreboot\nshutdown\nexit qtiles' | rofi -dmenu -i -p "[ Computer ] : " -hide-scrollbar -tokenize -lines 4 -eh 1 -width 40 -location 8 -xoffset 170 -yoffset 70 -padding 30 -disable-history -font "SourceCodePro 18")
+
+echo "This result is : $DIALOG_RESULT"
+sleep 1;
+
+if [ "$DIALOG_RESULT" = "exit qtiles" ];
+    then
+        kill -9 $(pgrep qtile)
+    #   kill -9 $(pidof -x qtile)
+    #   killall qtile
+elif [ "$DIALOG_RESULT" = "hibernate" ];
+    then
+        exec systemctl hibernate
+elif [ "$DIALOG_RESULT" = "reboot" ];
+    then
+        exec systemctl reboot
+elif [ "$DIALOG_RESULT" = "shutdown" ];
+    then
+        exec systemctl poweroff
+fi
