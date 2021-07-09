8de47fb33b0536c8730901d2cf11650831bcbd6e
Bhavuk <bhavuksharma2202@gmail.com>
2021-07-09 14:25:37 +0530

    _

--- power.sh
+++ power.sh
@@ -10,12 +10,12 @@ status=$(cat /sys/class/power_supply/AC0/online)
 #else
     if [[ $status -eq 0 ]]; then 
         su bhavuksharma2202 -c "DBUS_SESSION_BUS_ADDRESS=\"unix:path=/run/user/1000/bus\" notify-send \"Power\" \"AC Offline\""
-        /usr/bin/sudo -u bhavuksharma2202 /usr/bin/aplay /home/bhavuksharma2202/scripts/sounds/disconnected.wav > /dev/null 2>&1
+#        /usr/bin/sudo -u bhavuksharma2202 /usr/bin/aplay /home/bhavuksharma2202/scripts/sounds/disconnected.wav > /dev/null 2>&1
         rm /tmp/check
     else
         su bhavuksharma2202 -c "DBUS_SESSION_BUS_ADDRESS=\"unix:path=/run/user/1000/bus\" notify-send \"Power\" \"AC Online\""
-        /usr/bin/sudo -u bhavuksharma2202 /usr/bin/aplay /home/bhavuksharma2202/scripts/sounds/charging_activated.wav > /dev/null 2>&1
-        rm /tmp/check
+#        /usr/bin/sudo -u bhavuksharma2202 /usr/bin/aplay /home/bhavuksharma2202/scripts/sounds/charging_activated.wav > /dev/null 2>&1
+#        rm /tmp/check
     fi
 
 #fi
--- status/Click/cpu_status.sh
+++ status/Click/cpu_status.sh
@@ -4,6 +4,7 @@
 
 cpu_rpm=$(sensors | grep cpu_fan | awk '{print $2 " " $3}')
 temp=$(sensors | grep -E 'Core 0|Core 1|Core 2|Core 3'|awk '{print $1" "$2" "$3" "$4}')
+freq=$(sudo dmesg | grep MHz | sed -n 1p | awk {'print $5'})
 
-notify-send --icon $HOME/scripts/status/Click/icons/cpu.png "CPU" "$temp\n\nFan RPM: $cpu_rpm"
+notify-send --icon $HOME/scripts/status/Click/icons/cpu.png "CPU" "\nCPU Freq: $freq\n\n$temp\n\nFan RPM: $cpu_rpm"
 
