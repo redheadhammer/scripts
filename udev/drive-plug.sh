b669c535b0e0db8fa8e702763cc6dce25ed8909f
Bhavuk <bhavuksharma2202@gmail.com>
2021-07-15 22:15:04 +0530

    untracked power.sh and drive-plug.sh

--- udev/drive-plug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
-DISPLAY=":0"
-XAUTHORITY="/home/bhavuksharma2202/.Xauthority"
-
-su bhavuksharma2202 -c "DBUS_SESSION_BUS_ADDRESS=\"unix:path=/run/user/1000/bus\" notify-send \"Disks\" \"New Device Connected\""
-
