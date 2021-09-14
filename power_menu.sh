903282827eedda7563f26dd98b5ab55a188d57a2
Bhavuk <bhavuksharma2202@gmail.com>
2021-09-14 00:27:50 +0000

    changed offset for power_menu

--- power_menu.sh
+++ power_menu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIALOG_RESULT=$(echo -e 'hibernate\nreboot\npoweroff\nexit qtiles' | rofi -dmenu -i -p "[ Computer ] : " -hide-scrollbar -tokenize -lines 4 -eh 1 -width 40 -location 8 -xoffset 170 -yoffset 70 -padding 30 -disable-history -font "SourceCodePro 18")
+DIALOG_RESULT=$(echo -e 'hibernate\nreboot\npoweroff\nexit qtiles' | rofi -dmenu -i -p "[ Computer ] : " -hide-scrollbar -tokenize -lines 4 -eh 1 -width 40 -location 8 -xoffset 500 -yoffset 70 -padding 30 -disable-history -font "SourceCodePro 18")
 
 echo "This result is : $DIALOG_RESULT"
 sleep 1;
